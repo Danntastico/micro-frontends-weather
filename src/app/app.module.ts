467f2cf3951d8c885d869f1de735952b489f3862
Danilo Peña Mac <nicolasdpf@gmail.com>
2020-03-25 13:45:45 -0500

    Deleting components that aren't useful

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {APP_BASE_HREF} from '@angular/common';
-import { ListComponent } from './list/list.component';
-import { DetailComponent } from './detail/detail.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ListComponent,
-    DetailComponent,
     EmptyRouteComponent
   ],
   imports: [
