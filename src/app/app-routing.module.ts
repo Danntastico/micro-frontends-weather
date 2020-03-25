b7481e0ecd688c99e1794dd64c8c81b306342848
Danilo Peña Mac <nicolasdpf@gmail.com>
2020-03-25 15:10:30 -0500

    Adding Widget component in app.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {ListComponent} from './list/list.component';
-import {DetailComponent} from './detail/detail.component';
 import {EmptyRouteComponent} from './empty-route/empty-route.component';
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherWidgetMainComponent } from './Components/weather-widget-main/weather-widget-main.component';
 
 import {APP_BASE_HREF} from '@angular/common';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    EmptyRouteComponent
+    EmptyRouteComponent,
+    WeatherWidgetMainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [
     {provide: APP_BASE_HREF, useValue: '/'}
